809b19798b1d255f5f35513fd001a024ee277c70
thinkasany <117748716+thinkasany@users.noreply.github.com>
2023-09-04 10:15:14 +0800

    feat: Generate images and json (#1)

--- app/action.js
+++ app/action.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 const fs = require("fs");
+const { Octokit } = require("@octokit/core");
+const puppeteer = require("puppeteer");
 
-const asyncFunc = async orgName => {
+/** 生成数组 **/
+const outputArray = async orgName => {
   const apiUrl = `https://api.github.com/orgs/${orgName}/repos`;
   const allRepos = await axios.get(apiUrl);
   const repos = allRepos.data.map(i => i.full_name);
@@ -54,17 +57,172 @@ const asyncFunc = async orgName => {
   return contributorsArray;
 };
 
-export const Action = async orgName => {
-  const contributors = await asyncFunc(orgName);
-  console.log(contributors, contributors.length);
-  const jsonStr = JSON.stringify(contributors, null, 2);
+/** 生成图片 */
+const outputPng = async contributors => {
+  const avatars = contributors.map(i => i.avatar);
+  // https://github.com/puppeteer/puppeteer/issues/10729
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: `/usr/bin/google-chrome`,
+    args: [
+      `--no-sandbox`,
+      `--headless`,
+      `--disable-gpu`,
+      `--disable-dev-shm-usage`
+    ]
+  });
+  const page = await browser.newPage();
+  const customStyle = `
+    .custom {
+        width: 60px;
+        height: 60px;
+        margin: 1px 1px;
+        border-radius: 50%;
+    }
+`;
+  // 创建一个HTML字符串，其中包含多个图片
+  const htmlContent = `
+    <style>${customStyle}</style>
+    ${avatars
+      .map(avatarUrl => `<img src="${avatarUrl}" class="custom" />`)
+      .join("")}
+`;
 
-  // 写入 JSON 文件
-  fs.writeFile("data.json", jsonStr, "utf8", err => {
-    if (err) {
-      console.error("Error writing JSON file:", err);
-    } else {
-      console.log("JSON file has been saved.");
+  // 设置页面内容
+  await page.setContent(htmlContent);
+
+  // 等待加载所有图片
+  await page.waitForSelector("img");
+
+  // 修改容器的尺寸和样式
+  await page.evaluate(() => {
+    const container = document.body; // 或者选择包含所有图片的父元素
+    container.style.width = "890px";
+  });
+
+  // 获取包含所有图片的容器的尺寸
+  const containerSize = await page.evaluate(() => {
+    const container = document.body;
+    const rect = container.getBoundingClientRect();
+    return { width: rect.width, height: rect.height + 10 };
+  });
+
+  // 设置页面尺寸以适应容器
+  await page.setViewport(containerSize);
+
+  // 截取整个页面，包括所有图片
+  await page.screenshot({ path: "contributors.png", omitBackground: true });
+
+  await browser.close();
+  console.log("PNG文件已保存");
+};
+
+const Action = async payload => {
+  const {
+    orgName,
+    repo,
+    owner,
+    branch,
+    token,
+    jsonPath,
+    pngPath,
+    commitMessage,
+    committerName,
+    committerEmail
+  } = payload;
+  console.log("payload in action", payload);
+  const octokit = new Octokit({
+    auth: token
+  });
+  console.log("octokit", octokit);
+  const contributors = await outputArray(orgName);
+  let imageContent;
+  try {
+    await outputPng(contributors);
+    imageContent = fs.readFileSync("contributors.png");
+  } catch (error) {
+    console.log("图片生成遇到了问题", error);
+  }
+
+  // 创建一个 axios 实例，包含共享的请求配置
+  const Axios = axios.create({
+    baseURL: `https://api.github.com/repos/${owner}/${repo}`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
     }
   });
+
+  try {
+    // 1. 获取特定分支的最后一次提交 SHA
+    const branchResponse = await Axios.get(`/branches/${branch}`);
+    const lastCommitSHA = branchResponse.data.commit.sha;
+    console.log("lastCommitSHA", lastCommitSHA);
+
+    // 2. 创建 Blobs（base64 编码）
+    const createBlob = async (content, encoding) => {
+      const blobResponse = await Axios.post("/git/blobs", {
+        content: content,
+        encoding: encoding
+      });
+      return blobResponse.data.sha;
+    };
+
+    const jsonSHA = await createBlob(
+      Buffer.from(JSON.stringify(contributors)).toString("base64"),
+      "base64"
+    );
+    const pngSHA = await createBlob(imageContent.toString("base64"), "base64");
+
+    // 3. 创建一个定义了文件夹结构的树
+    const createTree = async (baseTreeSHA, blobs) => {
+      const tree = blobs.map(blob => {
+        return {
+          path: blob.path,
+          mode: "100644",
+          type: "blob",
+          sha: blob.sha
+        };
+      });
+
+      const treeResponse = await Axios.post("/git/trees", {
+        base_tree: baseTreeSHA,
+        tree: tree
+      });
+      return treeResponse.data.sha;
+    };
+
+    const treeSHA = await createTree(lastCommitSHA, [
+      { path: pngPath, sha: pngSHA },
+      { path: jsonPath, sha: jsonSHA }
+    ]);
+    console.log("treeSHA", treeSHA);
+
+    // 4. 创建提交
+    const createCommit = async treeSHA => {
+      const commitResponse = await Axios.post("/git/commits", {
+        message: commitMessage,
+        author: {
+          name: committerName,
+          email: committerEmail
+        },
+        parents: [lastCommitSHA],
+        tree: treeSHA
+      });
+      return commitResponse.data.sha;
+    };
+
+    const newCommitSHA = await createCommit(treeSHA);
+
+    // 5. 更新分支引用
+    await Axios.patch(`/git/refs/heads/${branch}`, {
+      sha: newCommitSHA
+    });
+  } catch (error) {
+    console.log("遇到错误", error);
+  }
+
+  console.log("files has been committed to the repository.");
 };
+
+module.exports = Action;
--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const Action = require("./app/action");
 const log = require("./app/log");
 const core = require("@actions/core");
+const github = require("@actions/github");
 
 (async function() {
   try {
     // 获取输入
+    const context = github.context;
+    const owner = context.repo.owner;
+    const repoName = context.repo.repo;
     const orgName = core.getInput("orgName", { required: true });
-    await Action(orgName);
-    log.info("CordCloud-JS Action 成功结束运行！", orgName);
+    const token = core.getInput("github_token", { required: true });
+    log.info(`orgName: ${orgName}; repoName: ${repoName}; owner:${owner};`);
+    const commitMessage =
+      core.getInput("commit_message") || "chore: update contributors [skip ci]"; // git commit message
+    const pngPath = core.getInput("png_Path") || "contributors.png";
+    const jsonPath = core.getInput("json_path") || "json/data.json";
+    const branch = core.getInput("branch") || "master";
+    const committerName = core.getInput("committer_name") || "contributors bot";
+    const committerEmail =
+      core.getInput("committer_email") || "action@gmail.com";
 
-
-    // log.warning("CordCloud Action 运行失败！");
+    const payload = {
+      orgName,
+      repo: repoName,
+      owner,
+      branch,
+      token,
+      jsonPath,
+      pngPath,
+      commitMessage,
+      committerEmail,
+      committerName
+    };
+    await Action(payload);
+    log.info("organize-contributors Action 成功结束运行！", orgName);
   } catch (error) {
     log.setFailed("错误：", error);
   }
